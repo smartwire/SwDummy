b5e98617edbacbaf589fd51d6bc40df573f63db8
unknown <t.kluge@sys003.smartwire.loc>
2015-05-12 12:34:54 +0200

    FIX class name CHN added more logging

--- SwDummy/module.php
+++ SwDummy/module.php
@@ -1,6 +1,6 @@
 <?
 
-	class SwDummy extends IPSModule
+	class SwHelloWorld extends IPSModule
 	{
 		
 		public function __construct($InstanceID)
@@ -8,6 +8,8 @@
 			//Never delete this line!
 			parent::__construct($InstanceID);
 			
+			IPS_LogMessage("SwHelloWorld->__construct() invoked with InstanceID", $InstanceID);
+			var_dump($this);
 		}
 		
 		public function ApplyChanges()
@@ -15,11 +17,13 @@
 			//Never delete this line!
 			parent::ApplyChanges();
 			
+			IPS_LogMessage("SwHelloWorld->ApplyChanges() invoked");
+			var_dump($this);
 		}
 		
 		public function HelloWorld($message)
 		{
-			IPS_LogMessage("SmartWire SwDummy->HelloWorld() invoked with message ", $message);
+			IPS_LogMessage("SwHelloWorld->HelloWorld() invoked with message ", $message);
 		}
 		
 
